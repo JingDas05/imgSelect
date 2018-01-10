468af517e11bda80f2d3a5ab134e8b2eb6856062
wusi06 <wusi0109@163.com>
2018-01-10 11:29:33 +0800

    添加常用方法定义

--- core/img/js/img.js
+++ core/img/js/img.js
@@ -3,14 +3,36 @@
  */
 
 var imgSelect = function () {
-    var start = 'start';
-    this.init(start);
+
+    function imgMouseDown(event) {
+        console.log('start');
+        $("p").click(function () {
+            console.log('click')
+        });
+    }
+
+    function imgMouseMove(event) {
+
+    }
+
+    function imgMouseUp(event) {
+
+    }
+
+    this.init();
+    $("p").click(function () {
+        console.log('outer-click')
+    });
 };
 
 imgSelect.prototype = {
 
     init: function () {
 
+    },
+
+    polygon: function (polygonId) {
+        var polygonEntity = $('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">');
     }
 };
 
